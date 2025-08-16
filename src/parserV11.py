1633d54cec595ea35e633729e1cf014c1ffab5a1
kayabaakihiko13 <iqbalramad75@gmail.com>
2025-08-17 03:18:54 +0700

    update: update intial value untuk confidence klasifikasi dan deteksi objek

--- src/parserV11.py
+++ src/parserV11.py
@@ -3,32 +3,44 @@ import onnxruntime as ort
 import yaml
 import matplotlib.pyplot as plt
 import cv2
-import time
+
+from typing import Any,List
 
 class YOLODetector:
-    def __init__(self, model_path, class_yaml, conf_thresh=0.5, iou_thresh=0.45):
+    def __init__(self, model_path:str, label_yaml:str,
+                 conf_thresh:float=0.25, iou_thresh:float=0.7):
+        """
+        this class purpose to vanila parser with onnxruntime ecosystem
+        model_path:str = path file model onnx file
+        label_yaml:str = path file untuk yaml file
+        conf_thresh:float = nilai minum untuk konfinde klasifikasi objek
+        iou_thresh:float = nilai minum untuk konfiden deteksi objek
+
+        source:
+        https://docs.ultralytics.com/modes/predict/#inference-arguments
+        """
         self.conf_thresh = conf_thresh
         self.iou_thresh = iou_thresh
 
         # Load classes from YAML
-        with open(class_yaml, 'r') as f:
+        with open(label_yaml, 'r') as f:
             self.CLASSES = yaml.safe_load(f)['names']
         self.COLORS = np.random.uniform(0, 255, size=(len(self.CLASSES), 3))
 
         # Load model
-        self.session = ort.InferenceSession(model_path)
+        self.session = ort.InferenceSession(model_path,providers=['CPUExecutionProvider'])
         input_shape = self.session.get_inputs()[0].shape
         self.INPUT_H = input_shape[2]
         self.INPUT_W = input_shape[3]
 
-    def preprocess(self, image):
+    def preprocess(self, image:np.ndarray)->np.ndarray:
         img_resized = cv2.resize(image, (self.INPUT_W, self.INPUT_H))
         img_rgb = cv2.cvtColor(img_resized, cv2.COLOR_BGR2RGB)
         img_norm = img_rgb.astype(np.float32) / 255.0
         img_chw = np.transpose(img_norm, (2, 0, 1))
         return np.expand_dims(img_chw, axis=0)
 
-    def postprocess(self, outputs, orig_h, orig_w):
+    def postprocess(self, outputs:Any, orig_h:int, orig_w:int):
         preds = outputs[0]
         preds = np.transpose(preds, (0, 2, 1))[0]
         boxes_xywh = preds[:, :4]
@@ -56,7 +68,7 @@ class YOLODetector:
                 final_class_ids.append(class_ids[i])
         return final_boxes, final_scores, final_class_ids
 
-    def detect(self, image):
+    def detect(self, image:np.ndarray)->List:
         orig_h, orig_w = image.shape[:2]
         img_tensor = self.preprocess(image)
         outputs = self.session.run(None, {self.session.get_inputs()[0].name: img_tensor})
